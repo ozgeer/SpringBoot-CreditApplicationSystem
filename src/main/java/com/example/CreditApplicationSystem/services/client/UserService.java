e14e15f0c55c20e38303c2013d895b7cc987d081
ozgeer <ozgee_err@hotmail.com>
2022-12-27 03:25:53 -0800

    added methods

--- src/main/java/com/example/CreditApplicationSystem/services/client/UserService.java
+++ src/main/java/com/example/CreditApplicationSystem/services/client/UserService.java
@@ -3,12 +3,13 @@ package com.example.CreditApplicationSystem.services.client;
 import com.example.CreditApplicationSystem.dto.client.UserCreateDTO;
 import com.example.CreditApplicationSystem.dto.client.UserUpdateDTO;
 import com.example.CreditApplicationSystem.dto.client.UserViewDTO;
+import com.example.CreditApplicationSystem.entities.Client;
 
 import java.util.List;
 
 public interface UserService {
     UserViewDTO createClient(UserCreateDTO userCreateDTO);
-    UserViewDTO updateClient(Integer Id,UserUpdateDTO userUpdateDTO);
-    UserViewDTO deleteClient(Integer Id);
+    UserViewDTO updateClient(Long Id,UserUpdateDTO userUpdateDTO);
+    UserViewDTO deleteClient(Long Id);
     List<UserViewDTO> getClientList();
 }
