98b785fc8e2afdb23b972cda451302fa3e19be9c
ozgeer <ozgee_err@hotmail.com>
2022-12-27 03:23:07 -0800

    added method

--- src/main/java/com/example/CreditApplicationSystem/repository/CreditRepository.java
+++ src/main/java/com/example/CreditApplicationSystem/repository/CreditRepository.java
@@ -1,9 +1,11 @@
 package com.example.CreditApplicationSystem.repository;
 
+import com.example.CreditApplicationSystem.entities.Client;
 import com.example.CreditApplicationSystem.entities.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CreditRepository extends JpaRepository<Credit,Integer> {
+    Credit findCreditByClient(Client client);
 }
