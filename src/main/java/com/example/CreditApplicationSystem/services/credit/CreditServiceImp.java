c3c536b87ff1713b7fd9a3072a457b54c62aa5da
ozgeer <ozgee_err@hotmail.com>
2023-01-05 10:12:30 -0800

    it was arranged

--- src/main/java/com/example/CreditApplicationSystem/services/credit/CreditServiceImp.java
+++ src/main/java/com/example/CreditApplicationSystem/services/credit/CreditServiceImp.java
@@ -1,30 +1,40 @@
 package com.example.CreditApplicationSystem.services.credit;
+import com.example.CreditApplicationSystem.constants.Response;
 import com.example.CreditApplicationSystem.dto.credit.CreditApplyDTO;
 import com.example.CreditApplicationSystem.dto.credit.CreditApplyResponseDTO;
+import com.example.CreditApplicationSystem.entities.ApprovedCredit;
 import com.example.CreditApplicationSystem.entities.Client;
 import com.example.CreditApplicationSystem.entities.Credit;
+import com.example.CreditApplicationSystem.repository.ApprovedCreditRepository;
 import com.example.CreditApplicationSystem.repository.CreditRepository;
 import com.example.CreditApplicationSystem.services.client.UserServiceImp;
 import com.example.CreditApplicationSystem.services.givencredit.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
 
 
 @Service
 @RequiredArgsConstructor
+@Controller
 public class CreditServiceImp implements CreditService {
 
     private final CreditRepository creditRepository;
     private final UserServiceImp userServiceImp;
+    private final ApprovedCreditRepository approvedCreditRepository;
 
-    public static Integer getCreditScoreCalculate(String citizenId){
-        Integer score=(int) (Math.random()*1500);
+    public static Integer getCreditScoreCalculate(String citizenId) {
+        Integer score = (int) (Math.random() * 1500);
         return Integer.valueOf(score);
     }
 
     @Override
     public void creditApply(CreditApplyDTO creditApplyDTO) {
-        Credit credit= new Credit();
+        Credit credit = new Credit();
         credit.setClient(creditApplyDTO.getClient());
         credit.setIncome(creditApplyDTO.getIncome());
         credit.setDeposit(creditApplyDTO.getDeposit());
@@ -32,45 +42,67 @@ public class CreditServiceImp implements CreditService {
         creditRepository.save(credit);
     }
 
-    public CreditApplyResponseDTO creditApplyEvaluation(String citizenId) {
-        Client client=userServiceImp.findCustomerByIdentityNo(citizenId);
-        Credit credit=creditRepository.findCreditByClient(client);
+    public CreditApplyResponseDTO creditApplyEvaluation(@RequestBody String citizenId) {
+        Client client = userServiceImp.findCustomerByIdentityNo(citizenId);
+        Credit credit = creditRepository.findCreditByClient(client);
 
         Double deposit = credit.getDeposit();
-        Double income=credit.getIncome();
-        Integer creditScore=credit.getCreditScore();
+        Double income = credit.getIncome();
+        Integer creditScore = credit.getCreditScore();
 
-        CreditApplyResponseDTO response=getCreditLimit(client,creditScore,income,deposit);
+        CreditApplyResponseDTO response = getCreditLimit(client, creditScore, income, deposit);
+        updateCreditTable(response);
         return response;
 
     }
 
-    public CreditApplyResponseDTO getCreditLimit(Client client, Integer creditScore, Double income, Double deposit){
-      Type type=new  Type();
-      if(creditScore<500){
-        type.setGivenCreditType(new CreditScoreBelowFiveHundred());
-      }
-      else if(creditScore>=500 && creditScore<1000 ){
-          if(income<5000) {
-              type.setGivenCreditType(new GivenCreditScoreBelowOneThousand());
-          }
-          else if(income>=5000 && income<10000){
-              type.setGivenCreditType(new GivenCreditIncomeBelowTenThousand());
-          }
-          else{
-              type.setGivenCreditType(new GivenCreditIncomeAboveTenThousand());
-          }
-      }
-      else {
-          type.setGivenCreditType(new GivenCreditScoreAboveOneThousand());
-      }
-        return type.getGivenCreditLimit(income,deposit);
+    public CreditApplyResponseDTO getCreditLimit(Client client, Integer creditScore, Double income, Double deposit) {
+        Type type = new Type();
+        if (creditScore < 500) {
+            type.setGivenCreditType(new CreditScoreBelowFiveHundred());
+        } else if (creditScore >= 500 && creditScore < 1000) {
+            if (income < 5000) {
+                type.setGivenCreditType(new GivenCreditScoreBelowOneThousand());
+            } else if (income >= 5000 && income < 10000) {
+                type.setGivenCreditType(new GivenCreditIncomeBelowTenThousand());
+            } else {
+                type.setGivenCreditType(new GivenCreditIncomeAboveTenThousand());
+            }
+        } else {
+            type.setGivenCreditType(new GivenCreditScoreAboveOneThousand());
+        }
+        return type.getGivenCreditLimit(client,income, deposit);
     }
 
-    public Credit findCreditByClient(Client client){
+    public Credit findCreditByClient(Client client) {
         return creditRepository.findCreditByClient(client);
     }
 
+    public void updateCreditTable(CreditApplyResponseDTO creditApplyResponseDTO) {
+        if (creditApplyResponseDTO.getCreditStatus() == Response.APPROVED) {
+            ApprovedCredit approvedCredit = new ApprovedCredit();
+            Credit credit=findCreditByClient(creditApplyResponseDTO.getClient());
+            approvedCredit.setClient(creditApplyResponseDTO.getClient());
+            approvedCredit.setCredit(credit);
+            approvedCredit.setDeposit(credit.getDeposit());
+            approvedCredit.setIncome(credit.getIncome());
+            approvedCredit.setCreditScore(credit.getCreditScore());
+            approvedCredit.setCreditStatus(creditApplyResponseDTO.getCreditStatus());
+            approvedCredit.setCreditLimit(creditApplyResponseDTO.getCreditLimit());
 
+            credit.setCreditLimit(creditApplyResponseDTO.getCreditLimit());
+            credit.setCreditStatus(creditApplyResponseDTO.getCreditStatus());
+            approvedCreditRepository.save(approvedCredit);
+            creditRepository.save(credit);
 
+        }
+        else{
+        Credit credit = findCreditByClient(creditApplyResponseDTO.getClient());
+        credit.setCreditStatus(creditApplyResponseDTO.getCreditStatus());
+        credit.setCreditLimit(creditApplyResponseDTO.getCreditLimit());
+        creditRepository.save(credit);
+    }
 }
+}
+
+
